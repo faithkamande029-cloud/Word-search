b0d167e9b4a788681a2bae8f307949df3f29de2e
Faith Kamande <faithkamande029@gmail.com>
2026-04-14 14:47:07 +0300

    add:Text to speech

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-const definition = document.querySelector(".def");
+const definition = document.querySelector(".definition");
 const textToSpeech = document.getElementById("text-to-speech");
 const button = document.getElementById("s-btn");
 const clearHistory = document.getElementById("clear-history");
 const searchStore = document.getElementById("search-store");
 // const searchStore = document.getElementById("search-store")
 
-
-const history = {};
 
 button.addEventListener('click', (event) => {
     // prevents page from automaticly reloading
@@ -19,63 +17,37 @@ button.addEventListener('click', (event) => {
 
 
     fetch(`${baseUrl}${searchWord}`)
-    .then(response => {
-        if(!response.ok){
-            throw new Error("Word not found")
-        }
-        return response.json();
-
-    })
+    .then(response => response.json())   
     .then(data => {
-        const html = `
-                  <div id= "definition" class="definition">
-                <div class="def">
-                    <h4>${searchWord}</h4>
-                    <button><span class="material-symbols-outlined">text_to_speech</span></button>
+        const html = ` 
+            <div class="def">
+                <h4>${searchWord}</h4>
+                <button><span class="material-symbols-outlined">text_to_speech</span></button>
                 </div>
                 <div class="define">
-                    <div class="noun">
-                        <p>noun</p>
-                        <p>/Sample/</p>
-                    </div>
-                    <div class="defined-word">
-                        <p class="def-word">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt quo aut voluptatibus temporibus nisi maxime dicta necessitatibus tempore suscipit maiores quia minus, hic laboriosam architecto fugit obcaecati reiciendis quidem est?
-                        </p>
-                    </div>
-                    <div class="context">
-                        <p>Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div class="translate">
-                        <p><strong>Swahili</strong>: Lorem, ipsum dolor.</p>
-                    </div>
-                </div>                
-                
-            </div>
+                <div class="noun">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <div class="defined-word">
+                    <p class="def-word">
+                        <strong>Definition: </strong> ${data[0].meanings[0].definitions[0].definition}
+                    </p>
+                </div>
+                <div class="context">
+                    <strong>Sentence: </strong><p>${data[0].meanings[0].definitions[0].example}</p>
+                </div>
+                <div class="translate">
+                    <p><strong>Swahili</strong>: ${searchWord}</p>
+                </div>
+            </div>        
         `;
         definition.innerHTML = html;
+        sound.setAttribute(`"src", https:${data[0].phonetics[0].audio}`)
+        console.log(sound)
     })
     
-    .catch(error => console.error(error.message));
-
-    // const searchArrays = Array.prototype.slice.call(event.target.elements);
-
-    // searchArrays.forEach((element) => {
-
-    //     if (element.name) {
-    //         history[element.name] = element.value;
-    //     }
-        
-    // });
-
-    // localStorage.setItem('history', JSON.stringify(history));
+    .catch(error => console.error('Error, Word not found', error));
 })
-storeSearch.addEventListener("click", searchArrays);
 
-
-clearHistory.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    form.reset();
-})
-clearHistory(history)
+const history = {};
