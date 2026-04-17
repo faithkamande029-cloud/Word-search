409f4dfd9af0f57d5ca8acb12e0783cab50d9a90
Faith Kamande <faithkamande029@gmail.com>
2026-04-17 16:19:15 +0300

    error handling

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const url =  "https://random-word-api.herokuapp.com/word?number=5";
+const url = "https://random-word-api.herokuapp.com/word?number=5";
 
 const definition = document.querySelector("#definition");
 const button = document.getElementById("s-btn");
 const clearHistory = document.getElementById("clear-history");
-const searchStore = document.getElementById("search-store"), 
-    h2 = document.querySelector("h2");
+const searchStore = document.getElementById("search-store"),
+  h2 = document.querySelector("h2");
 
-h2.innerHTML = localStorage.getItem("value")
+h2.innerHTML = localStorage.getItem("value");
 
+button.addEventListener("click", (event) => {
+  // prevents page from automaticly reloading
+  event.preventDefault();
 
-button.addEventListener('click', (event) => {
-    // prevents page from automaticly reloading
-    event.preventDefault();     
+  let searchWord = searchStore.value;
 
-    let searchWord = searchStore.value;    
-
-    fetch(`${baseUrl}${searchWord}`)
-    .then(response => {
-        if(!response.ok){
-            throw new Error("HTTP error" + response.status)
-        }
-        return response.json();
-
-    })   
-    .then(data => {
-        if (data === null){
-            throw new Error("Word not found")
-        }
-        const html = ` 
+  fetch(`${baseUrl}${searchWord}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("HTTP error" + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data === null) {
+        throw new Error("Word not found");
+      }
+      const html = ` 
             <div class="def">
                 <h4>${searchWord}</h4>
                 <button id="sound" >
@@ -54,66 +52,67 @@ button.addEventListener('click', (event) => {
             </div> 
                   
         `;
-        definition.innerHTML = html;
+      definition.innerHTML = html;
 
+      //event listener for the text to speech button
+      const audio = document.getElementById("sound");
 
-        //event listener for the text to speech button
-        const audio = document.getElementById('sound');
+      audio.addEventListener("click", () => {
+        let audioLink = data[0].phonetics[0].audio;
 
-        audio.addEventListener("click", () => {
-            let audioLink = data[0].phonetics[0].audio;
+        if (audioLink) {
+          const speechAudio = new Audio(audioLink);
+          speechAudio.play().catch((error) => console.log(error));
+        } else {
+          alert("No audio available");
+        }
+      });
 
-            if (audioLink){
-                const speechAudio = new Audio(audioLink);
-                speechAudio.play().catch(error => console.log(error));
-            }else{
-                alert("No audio available")
-            }
-        });
-        
-        saveWordDisplay();      
-        
+      saveWordDisplay();
+      showError("");
     })
-    .catch(error => {        
-        showError(error.message);  
+    .catch((error) => {
+      showError(error.message);
     });
 });
 
+function showError(message) {
+  const errorDiv = document.querySelector(".error");
 
-function saveWordDisplay () {
-    localStorage.setItem('value', searchStore.value);
-    h2.innerHTML = localStorage.getItem("value") || "";
+  errorDiv.textContent = message;
 }
 
-clearHistory.addEventListener("click",(event) => {
-    event.preventDefault();
-    
-    //clears one item after the other in local storage
-    localStorage.removeItem("value");
-    h2.innerHTML = "";
-    
-    alert("History cleared");
+function saveWordDisplay() {
+  localStorage.setItem("value", searchStore.value);
+  h2.innerHTML = localStorage.getItem("value") || "";
+}
+
+clearHistory.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  //clears one item after the other in local storage
+  localStorage.removeItem("value");
+  h2.innerHTML = "";
+
+  alert("History cleared");
 });
 
-function getNewWord(){
-    const randomWord = document.getElementById("word-highlight");
+function getNewWord() {
+  const randomWord = document.getElementById("word-highlight");
 
-    fetch(url)
-    .then(response => response.json())
-    .then (data => {
-        const word = data[0];
-        const dataHtml = `
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const word = data[0];
+      const dataHtml = `
          <h5>Word of the day:</h5>
          <p class="word-posted">${word}</p>
          <button id="word-today">Get new word</button>
-        `; 
-        randomWord.innerHTML = dataHtml;        
+        `;
+      randomWord.innerHTML = dataHtml;
     })
-    .catch(error => 
-        {console.error("Invalid!",error);
+    .catch((error) => {
+      console.error("Invalid!", error);
     });
-    
 }
-document.getElementById("word-today")
-    .addEventListener("click", getNewWord);
-
+document.getElementById("word-today").addEventListener("click", getNewWord);
